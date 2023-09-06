5a77418604e2f14680e831c164d84ecade869c8f
Paul Flammarion <paul@flammarion.eu>
2023-09-06 16:23:08 +0200

    add rcv calculation

--- controller.py
+++ controller.py
@@ -1,11 +1,24 @@
-import shutil
-
-from PyQt6.QtWidgets import QPushButton, QFileDialog
+from PyQt6.QtWidgets import QFileDialog
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 class Controller:
     def __init__(self, view):
         self.view = view
+
+        self.technology_value = 45
+        self.voltage_value = 1.2
+
+        lam, G1, G2, Gap = self.parameters_init()
+        self.image_matrix = self.draw_layout(lam, G1, G2, Gap)
+        points = np.where(self.image_matrix != 0, 1, 0)
+        mask = self.calc_and_plot_RCV()
+
+        result = cv2.addWeighted(points, 1, mask, 0.5, 0)
+
+        self.view.display_image(result)
 
     def upload_image(self):
         file_dialog = QFileDialog()
@@ -15,16 +28,118 @@ class Controller:
         if file_dialog.exec():
             selected_files = file_dialog.selectedFiles()
             if selected_files:
-                source_file = selected_files[0]  # Assuming only one file is selected
-                destination_folder = "resources/"  # Change this to your resource folder path
-                shutil.copy(source_file, destination_folder)
-                print("Image uploaded")
+                # TODO check if only one file is selected or more
+                image_path = selected_files[0]
+                image_matrix = cv2.imread(image_path)
+                self.view.display_image(image_matrix)
+                print("Image loaded as a matrix")
 
-    def button_add_clicked(self):
-        print("this is working")
-        # Handle button 1 click event here
-        pass
+    def psf_xy(self, lam, na, x, y, xc, yc, radius_max=np.inf):
 
-    def button2_clicked(self):
-        # Handle button 2 click event here
-        pass
+        def std_dev(std_lam, std_na):
+            return 0.37 * std_lam / std_na
+
+        r = np.sqrt(np.square(x - xc) + np.square(y - yc))
+        ind = r <= radius_max
+
+        y = 1 / np.sqrt(2 * np.pi * np.square(std_dev(lam, na))) * np.exp(
+            -(np.square(x - xc) + np.square(y - yc)) / (2 * np.square(std_dev(lam, na))))
+
+        # Clear values outside of radius_max
+        y = y * ind
+
+        return y
+
+    def psf_2d_pos(self, fov, lam, na, xc, yc, clear_radius=np.inf):
+        s_x, s_y = (fov, fov)  # mat.shape
+        x, y = np.mgrid[0:s_x, 0:s_y]
+        mat = self.psf_xy(lam, na, x, y, xc, yc, clear_radius)
+        return mat
+
+    def psf_2d(self, fov, lam, na, clear_radius=np.inf):
+        s_x, s_y = (fov, fov)  # mat.shape
+        xc = s_x // 2
+        yc = s_y // 2
+        return self.psf_2d_pos(fov, lam, na, xc, yc, clear_radius)
+
+    def round(self, i):
+        return int(np.rint(i))
+
+    def draw_layout(self, lam, G1, G2, Gap):
+        layout = np.empty(shape=(1000, 1000))
+        layout.fill(0)
+
+        x = self.round(2 * lam)
+        y = self.round(4 * lam)
+        layout[0:x - 1, 0:y - 1].fill(0)
+
+        layout[x:x + self.round(4 * lam), 1:self.round(4 * lam)].fill(G1)
+
+        x = x + self.round(4 * lam)
+        y = self.round(4 * lam)
+        layout[x:x + self.round(12 * lam), 1:self.round(4 * lam)].fill(Gap)
+
+        x = x + self.round(12 * lam)
+        y = self.round(4 * lam)
+        layout[x:x + self.round(4 * lam), 1:self.round(4 * lam)].fill(0)
+        x = x + self.round(4 * lam)
+        layout[x:x + self.round(2 * lam), 1:self.round(4 * lam)].fill(G2)
+
+        # Try to center it in a larger field of view
+        layout_full = np.empty(shape=(3000, 3000))
+        layout_full.fill(0)
+        offset = [1250, 1450]
+        layout_full[offset[0]:offset[0] + layout.shape[0], offset[1]:offset[1] + layout.shape[1]] = layout
+
+        return layout_full
+
+    def parameters_init(self):
+        Kn = 1
+        Kp = -1.3
+        lam = self.technology_value / 2
+
+        Gate_N = self.voltage_value * Kn * 1E7
+        lam3_N = 4 * 3 * self.voltage_value * Kn
+        lam4_N = 4 * 4 * self.voltage_value * Kn
+        lam6_N = 4 * 6 ** self.voltage_value * Kn
+
+        Gate_P = self.voltage_value * Kp * 1E7
+        lam3_P = 4 * 3 * self.voltage_value * Kp
+        lam4_P = 4 * 4 * self.voltage_value * Kp
+        lam6_P = 4 * 6 * self.voltage_value * Kp
+
+        G1 = Gate_N
+        G2 = Gate_P
+        D1 = lam4_N
+        D2 = lam4_P
+        Gap = 0
+        return lam, G1, G2, Gap
+
+    def update_physics_values(self):
+        self.technology_value = int(self.view.get_input_tech())
+        self.voltage_value = int(self.view.get_input_voltage())
+
+        lam, G1, G2, Gap = self.parameters_init()
+        self.image_matrix = self.draw_layout(lam, G1, G2, Gap)
+        self.view.display_image(self.image_matrix)
+
+        print("Physic info were updated ", self.technology_value, " ", self.voltage_value)
+
+    def calc_and_plot_RCV(self, lam=1300, NA=0.75, is_confocal=True, offset=None):
+        FWHM = 1.22 / np.sqrt(2) * lam / NA
+        FOV = 3000
+        if not offset:
+            L = self.psf_2d(FOV, lam, NA, FWHM // 2 if is_confocal else np.inf)
+        else:
+            L = self.psf_2d_pos(FOV, lam, NA, offset[0], offset[1], FWHM // 2 if is_confocal else np.inf)
+
+        amp_abs = np.sum(self.image_matrix * L)
+
+        # This is only correct if the full laser spot is in L (if it is not truncated)
+        # if is_confocal:
+        num_pix_under_laser = np.sum(L > 0)
+
+        amp_rel = amp_abs / num_pix_under_laser
+        print("RCV (per nm²) = %.6f" % amp_rel)
+
+        return np.where(L > 0, 1, 0)
--- view.py
+++ view.py
@@ -1,12 +1,17 @@
-from PyQt6.QtGui import QPixmap
+import numpy as np
+from PyQt6.QtGui import QPixmap, QImage
 from PyQt6.QtWidgets import QMainWindow, QWidget, QGridLayout, QLabel, QPushButton, QVBoxLayout, QLineEdit
-
+import pyqtgraph as pg
 import controller
 
 
 class View(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.image_view = pg.ImageView()
+	# TODO add default values for the user
+        self.info_input_tech = QLineEdit(self)
+        self.info_input_voltage = QLineEdit(self)
         self.main_window = QMainWindow()
         self.controller = controller.Controller(self)
         self.init_ui()
@@ -34,47 +39,52 @@ class View(QMainWindow):
         # Creating the info widget
 
         info_widget = QWidget()
-        info_label = QLabel("Physics info", self)
-        info_input1 = QLineEdit(self)
-        info_input2 = QLineEdit(self)
+        info_label = QLabel("Physics info:", self)
+        info_label_tech = QLabel("Technology", self)
+        info_label_voltage = QLabel("Voltage", self)
+        info_button = QPushButton("Submit values", self)
+        info_button.clicked.connect(self.controller.update_physics_values)
 
         input_layout = QGridLayout(info_widget)
         input_layout.addWidget(info_label, 0, 0)
-        input_layout.addWidget(info_input1, 1, 0)
-        input_layout.addWidget(info_input2, 1, 1)
+        input_layout.addWidget(info_label_tech, 1, 0)
+        input_layout.addWidget(self.info_input_tech, 2, 0)
+        input_layout.addWidget(info_label_voltage, 1, 1)
+        input_layout.addWidget(self.info_input_voltage, 2, 1)
+        input_layout.addWidget(info_button, 3, 0)
 
         # Creating the selector widget
 
         selector_widget = QWidget()
+        selector_labelx = QLabel("x: ", self)
         selector_input1 = QLineEdit(self)
+        selector_labely = QLabel("y: ", self)
         selector_input2 = QLineEdit(self)
-        selector_button = QPushButton("Selector Button", self)
+        selector_button = QPushButton("change position", self)
 
-        selector_layout = QVBoxLayout(selector_widget)
-        selector_layout.addWidget(selector_input1)
-        selector_layout.addWidget(selector_input2)
-        selector_layout.addWidget(selector_button)
+        selector_layout = QGridLayout(selector_widget)
+        selector_layout.addWidget(selector_labelx, 0, 0)
+        selector_layout.addWidget(selector_input1, 0, 1)
+        selector_layout.addWidget(selector_labely, 1, 0)
+        selector_layout.addWidget(selector_input2, 1, 1)
+        selector_layout.addWidget(selector_button, 2, 1)
 
         # Creating the main widget
 
         main_btn_container_widget = QWidget()
         main_btn_container_layout = QGridLayout(main_btn_container_widget)
 
-        main_button1 = QPushButton("Button 1", self)
-        main_button2 = QPushButton("Button 2", self)
-        main_button3 = QPushButton("Button 3", self)
-        main_button4 = QPushButton("Button 4", self)
+        main_button1 = QPushButton("Show original output", self)
+        main_button2 = QPushButton("Calc RCV", self)
+        main_button3 = QPushButton("EOFM", self)
 
         main_btn_container_layout.addWidget(main_button1, 0, 0)
         main_btn_container_layout.addWidget(main_button2, 0, 1)
         main_btn_container_layout.addWidget(main_button3, 0, 2)
-        main_btn_container_layout.addWidget(main_button4, 0, 3)
 
-        image_label = QLabel()
-        image_label.setFixedSize(400, 400)
-        image = QPixmap('./resources/image.jpg')
-        image = image.scaled(image_label.size())
-        image_label.setPixmap(image)
+        self.image_view.ui.histogram.hide()
+        self.image_view.ui.roiBtn.hide()
+        self.image_view.ui.menuBtn.hide()
 
         text_label = QLabel("RCV :", self)
 
@@ -82,7 +92,7 @@ class View(QMainWindow):
         main_layout = QGridLayout(main_widget)
 
         main_layout.addWidget(main_btn_container_widget, 0, 0)
-        main_layout.addWidget(image_label, 1, 0)
+        main_layout.addWidget(self.image_view, 1, 0)
         main_layout.addWidget(text_label, 2, 0)
 
         # Add all to the window layout
@@ -92,3 +102,13 @@ class View(QMainWindow):
         layout.addWidget(selector_widget, 1, 0)
         layout.addWidget(main_widget, 1, 1)
 
+    def display_image(self, image_matrix):
+        # TODO find a nicer solution to display images
+        image_matrix = np.rot90(image_matrix)
+        self.image_view.setImage(image_matrix)
+
+    def get_input_tech(self):
+        return self.info_input_tech.text()
+
+    def get_input_voltage(self):
+        return self.info_input_voltage.text()
