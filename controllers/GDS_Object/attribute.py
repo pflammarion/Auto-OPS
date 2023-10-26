a50fe25e5391c8bea9820baa34ad3db1b526908c
Paul Flammarion <paul@flammarion.eu>
2023-10-26 14:31:24 +0200

    add class documentation to gds drawing objects

--- controllers/GDS_Object/attribute.py
+++ controllers/GDS_Object/attribute.py
@@ -1,4 +1,21 @@
 class Attribute:
+    """
+    Represents a shape attribute with specific characteristics.
+
+    Attributes:
+        shape_type (ShapeType): The type of shape associated with the attribute.
+        label (str): A label that can be assigned to the attribute (optional).
+        state (bool): The state of the attribute, which can represent various conditions or statuses (optional).
+
+    Methods:
+        __init__(shape_type, label=None, state=None):
+            Initializes a new instance of the Attribute class with the provided parameters.
+
+    Usage:
+        # Creating an instance of the Attribute class
+        my_attribute = Attribute(ShapeType.METAL', label='A1', state=True)
+    """
+
     def __init__(self, shape_type, label=None, state=None):
         self.shape_type = shape_type
         self.label = label
--- main.py
+++ main.py
@@ -160,9 +160,9 @@ if __name__ == "__main__":
 
         cell_to_fix = ['ANTENNA_X1', 'CLKGATETST_X1', 'CLKGATETST_X2', 'CLKGATETST_X4', 'CLKGATETST_X8', 'CLKGATE_X1', 'CLKGATE_X2', 'CLKGATE_X4', 'CLKGATE_X8', 'DFFRS_X2', 'DFFR_X2', 'DFFS_X2', 'SDFFRS_X1', 'SDFFR_X1', 'SDFFR_X2', 'SDFF_X2', 'WELLTAP_X1']
 
-        prefix = 'SDFFR_X2'
+        prefix = 'NAND2_X2'
 
-        filtered_cells = [cell for cell in cell_to_fix if cell.startswith(prefix)]
+        filtered_cells = [cell for cell in cells if cell.startswith(prefix)]
         #filtered_cells = [prefix]
         is_debug = True
         error_cell_list = []
