3b1bbc79edf5ef3941869869ab16f6f02a745dc8
Paul Flammarion <paul@flammarion.eu>
2023-11-30 09:51:19 +0100

    implement test cases

---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+source venv/bin/activate
+
+python3 ./main.py\
+                --std_file input/stdcells.gds \
+                --lib_file input/stdcells.lib \
+                --layer_list "[[1, 0], [5, 0], [9, 0], [10, 0], [11, 0], [11, 0]]" \
+                --cell_list INV_X1 XOR2_X1 \
+                --output unit_test
