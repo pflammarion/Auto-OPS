71e44c9a247746f88c609e3b504b3962a8193501
Paul Flammarion <paul@flammarion.eu>
2023-12-18 15:16:21 +0100

    start.sh for clock

--- start.sh
+++ start.sh
@@ -1,12 +1,11 @@
-#!/bin/zsh
+#!/bin/sh
 source venv/bin/activate
 
 python3 ./main.py\
                 --std_file input/stdcells.gds \
                 --lib_file input/stdcells.lib \
-                --layer_list 1 5 9 10 11 \
-                --input 1 0 \
-                --cell_list XOR2_X1\
+                --cell_list DFF_X1 \
+                --layer_list "[[1, 0], [5, 0], [9, 0], [[10, 0]], [[11, 0]], [[11, 0]]]" \
                 --output reflection_over_cell\
                 --verbose
 : <<'END_COMMENT'
