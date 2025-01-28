bb6f1cc5b2a16a01e4cb2d601c694afeafd1e274
pflammarion <pflammarion@users.noreply.github.com>
2025-01-28 06:04:24 +0000

    deploy: c0f65029d214243c654f2ad3541051a71ed25727

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-source venv/bin/activate
-
-python3 ./main.py\
-                --std_file input/stdcells.gds \
-                --lib_file input/stdcells.lib \
-                --layer_list "[[1, 0], [5, 0], [9, 0], [[10, 0]], [[11, 0]], [[11, 0]]]" \
-                --unit_test 45
